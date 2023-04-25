e2159313f2afb15d30fbc1651a2ee0332a39bd1c
Miguel Martinez <miguel.emezero@gmail.com>
2023-04-25 14:15:16 -0600

    👾 feat: config files for project

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+    'react-hooks'
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-floating-promises': 'off'
+  }
+}
